cb01a5df4577ac921a411b626665d22322a7f553
Adam Werries <werries05@gmail.com>
2015-01-27 02:08:15 -0500

    lol i'm stupid and didn't read

--- import_keys.py
+++ import_keys.py
@@ -50,7 +50,7 @@ def main(argv):
             conf_file = arg
     print('Input file is "', conf_file)
     conf = read_conf_file(conf_file)
-    auth_data = pull_auth_data(conf)
+    auth_data = pull_allianceauth_data(conf)
     seat_data = pull_seat_data(conf)
     return conf,auth_data,seat_data
 
